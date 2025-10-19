5b8b657e65a998780e1e585a391a1b026e6ee3db
shahryar <maharshahryar518@gmail.com>
2025-10-19 17:21:57 +0500

    fourth commit

--- lib/api.ts
+++ lib/api.ts
@@ -22,6 +22,7 @@ const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
+    'ngrok-skip-browser-warning': 'true', // Skip ngrok browser warning
   },
 });
 
